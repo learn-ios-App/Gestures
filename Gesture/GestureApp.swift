dbff400d0775de7ad722f6d14bc83b0cef91ecd8
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2024-02-07 11:19:55 +0900

    Initial Commit

---
+++ Gesture/GestureApp.swift
@@ -0,0 +1,17 @@
+//
+//  GestureApp.swift
+//  Gesture
+//
+//  Created by 渡邊魁優 on 2024/02/07.
+//
+
+import SwiftUI
+
+@main
+struct GestureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
