1b83855c48b7dca274fd8b68304f52ee89920f9c
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2024-02-07 14:51:06 +0900

    commit

--- Gesture/ContentView.swift
+++ Gesture/ContentView.swift
@@ -8,17 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var rectAngle = ActiveRectAngle(
+    @StateObject private var rectAngle = ActiveRectAngle(
         sizes: [CGSize(width: 150, height: 80),
                 CGSize(width: 200, height: 40),
                 CGSize(width: 50, height: 250),
                 CGSize(width: 220, height: 100),
                 CGSize(width: 90, height: 90)])
+    
     var body: some View {
         VStack {
             Spacer()
             Rectangle()
-                .tint(.orange)
+                .foregroundStyle(rectAngle.color)
                 .frame(
                     width: rectAngle.size.width,
                     height: rectAngle.size.height
@@ -29,9 +30,6 @@ struct ContentView: View {
                 .gesture(rectAngle.longPressGesture)
                 .gesture(rectAngle.tapGesture)
             Spacer()
-        }
-        .onAppear() {
-            print("Hello")
         }
     }
 }
